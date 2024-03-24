ca49d740176a696225b5944c45986b41b9893c7c
Diego Garcia Cerdas <diego.gcerdas@gmail.com>
2024-03-24 08:09:02 +0100

    Fixes to alignprop

--- alignprop.py
+++ alignprop.py
@@ -10,6 +10,8 @@ from peft.utils import get_peft_model_state_dict
 from diffusers.loaders import LoraLoaderMixin
 from utils.img_utils import save_images
 from torchvision import transforms
+import random
+import torch.utils.checkpoint as checkpoint
 
 def run(
     args_pretrained_model_name_or_path: str,
@@ -17,7 +19,6 @@ def run(
     args_num_timesteps: int,
     args_guidance_scale: float,
     args_lora_rank: int,
-    args_lora_alpha: int,
     args_train_text_encoder: bool,
     args_learning_rate: float,
     args_batch_size: int,
@@ -32,7 +33,7 @@ def run(
 ):
     pl.seed_everything(args_seed)
     
-    pipeline = StableDiffusionPipeline.from_pretrained(args_pretrained_model_name_or_path).to(args_device)
+    pipeline = StableDiffusionPipeline.from_pretrained(args_pretrained_model_name_or_path)
     
     # freeze parameters of models to save more memory
     pipeline.vae.requires_grad_(False)
@@ -49,7 +50,7 @@ def run(
     # Now we will add new LoRA weights to the attention layers
     unet_lora_config = LoraConfig(
         r=args_lora_rank,
-        lora_alpha=args_lora_alpha,
+        lora_alpha=args_lora_rank,
         init_lora_weights="gaussian",
         target_modules=[
             "to_k",
@@ -61,16 +62,18 @@ def run(
         ],
     )
     pipeline.unet.add_adapter(unet_lora_config)
+    pipeline.unet.train()
 
     # The text encoder comes from 🤗 transformers, we will also attach adapters to it.
     if args_train_text_encoder:
         text_lora_config = LoraConfig(
             r=args_lora_rank,
-            lora_alpha=args_lora_alpha,
+            lora_alpha=args_lora_rank,
             init_lora_weights="gaussian",
             target_modules=["q_proj", "k_proj", "v_proj", "out_proj"],
         )
-        pipeline.text_encoder.add_adapter(text_lora_config)     
+        pipeline.text_encoder.add_adapter(text_lora_config)    
+        pipeline.text_encoder.train() 
     
     # Initialize the optimizer
     params_to_optimize = list(filter(lambda p: p.requires_grad, pipeline.unet.parameters()))
@@ -103,34 +106,36 @@ def run(
         ).input_ids.to(args_device)
     )[0]
     cond_prompt_embeds = prompt_embeds.repeat(args_batch_size, 1, 1) 
-
-    pipeline.train()
             
     for iter_i in list(range(args_num_train_iterations)):
             
-        latent = torch.randn((args_batch_size, 4, 64, 64), device=args_device)           
-    
-        with torch.enable_grad(): # important b/c don't have on by default in module                        
+        latent = torch.randn((args_batch_size, 4, 64, 64), device=args_device)                                
 
-            for t in tqdm(pipeline.scheduler.timesteps, total=len(pipeline.scheduler.timesteps)):
+        for i, t in tqdm(enumerate(pipeline.scheduler.timesteps), total=len(pipeline.scheduler.timesteps)):
 
-                t = torch.tensor([t], device=args_device).repeat(args_batch_size)
-                
-                noise_pred_uncond = pipeline.unet(latent, t, uncond_prompt_embeds).sample
-                noise_pred_cond = pipeline.unet(latent, t, cond_prompt_embeds).sample
-                grad = (noise_pred_cond - noise_pred_uncond)
-                noise_pred = noise_pred_uncond + args_guidance_scale * grad   
-             
-                latent = pipeline.scheduler.step(noise_pred, t[0].long(), latent).prev_sample
-                                    
-            ims = pipeline.vae.decode(latent.to(pipeline.vae.dtype) / 0.18215).sample
+            t = torch.tensor([t], device=args_device).repeat(args_batch_size)
+
+            noise_pred_uncond = checkpoint.checkpoint(pipeline.unet, latent, t, uncond_prompt_embeds, use_reentrant=False).sample
+            noise_pred_cond = checkpoint.checkpoint(pipeline.unet, latent, t, cond_prompt_embeds, use_reentrant=False).sample
+
+            timestep = random.randint(0, pipeline.scheduler.timesteps)
+            if i < timestep:
+                noise_pred_uncond = noise_pred_uncond.detach()
+                noise_pred_cond = noise_pred_cond.detach()
+
+            grad = (noise_pred_cond - noise_pred_uncond)
+            noise_pred = noise_pred_uncond + args_guidance_scale * grad   
             
-            loss = -loss_fn(ims, mode='train', no_grad=False).mean()
-            
-            # backward pass
-            loss.backward()
-            optimizer.step()
-            optimizer.zero_grad()    
+            latent = pipeline.scheduler.step(noise_pred, t[0].long(), latent).prev_sample
+                                
+        ims = pipeline.vae.decode(latent.to(pipeline.vae.dtype) / 0.18215).sample
+        
+        loss = -loss_fn(ims, mode='train', no_grad=False).mean()
+        
+        # backward pass
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()    
 
         if iter_i % args_validation_iters == 0:
             
@@ -183,10 +188,11 @@ def run(
             else:
                 text_encoder_state_dict = None
 
-            os.makedirs(args_save_folder, exist_ok=True)
-            save_images(images, args_save_folder)
+            save_folder = os.path.join(args_save_folder, f'Iters {iter_i}')
+            os.makedirs(save_folder, exist_ok=True)
+            save_images(images, save_folder)
             LoraLoaderMixin.save_lora_weights(
-                save_directory=args_save_folder,
+                save_directory=save_folder,
                 unet_lora_layers=unet_lora_state_dict,
                 text_encoder_lora_layers=text_encoder_state_dict,
             )          
