495c7d183b95ad19e7749d2f7bbb593e9bbb8445
Diego Garcia Cerdas <diego.gcerdas@gmail.com>
2024-04-08 05:15:38 +0200

    switch to concept discovery

--- concept_discovery_full.py
+++ concept_discovery_full.py
@@ -8,18 +8,18 @@ if __name__ == "__main__":
     )
 
     run(
-        args_pretrained_model_name_or_path="runwayml/stable-diffusion-v1-5",
+        args_pretrained_model_name_or_path="stabilityai/stable-diffusion-2",
         args_seed=0,
-        args_train_data_dir="./concept_discovery/data",
-        args_resolution=512,
+        args_train_data_dir="./lora_slider/max",
+        args_resolution=768,
         args_repeats=1,
         args_add_weight_per_score=True,
         args_init_weight=0.2,
-        args_learning_rate=5e-3,
+        args_learning_rate=1e-3,
         args_train_batch_size=2,
         args_device=device,
         args_num_train_epochs=50,
-        args_validation_epochs=10,
-        args_output_dir="./concept_discovery/output",
+        args_validation_epochs=5,
+        args_output_dir="./lora_slider/concept_discovery",
         args_num_validation_images=10,
     )
