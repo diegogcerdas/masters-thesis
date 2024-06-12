956ae3e2b9673ba237501ccb2546bca94ddb3f4f
Diego Garcia Cerdas <diego.gcerdas@gmail.com>
2024-06-12 14:45:35 +0200

    run dino for all rois and run measures. mod files

--- run_dino_part1.py
+++ run_dino_part1.py
@@ -22,41 +22,56 @@ def main(cfg):
         ]
     )
 
-    roi_names, roi_classes = parse_rois([cfg.roi])
-    roi_indices = get_roi_indices(os.path.join('/gpfs/work5/0/gusr53691/data/NSD/', f"subj{cfg.subject:02d}"), roi_names, roi_classes, cfg.hemisphere)
+    ckpt_path_left = os.path.join(cfg.ckpt_dir, 'dino_vit', f'0{cfg.subject}_all_l_all_0')
+    ckpt_path_left = os.path.join(ckpt_path_left, sorted(list(os.listdir(ckpt_path_left)))[-1])
+    ckpt_path_right = os.path.join(cfg.ckpt_dir, 'dino_vit', f'0{cfg.subject}_all_r_all_0')
+    ckpt_path_right = os.path.join(ckpt_path_right, sorted(list(os.listdir(ckpt_path_right)))[-1])
 
-    if cfg.hemisphere == 'left':
-        ckpt_path = os.path.join(cfg.ckpt_dir, 'dino_vit', f'0{cfg.subject}_all_l_all_0')
-    else:
-        ckpt_path = os.path.join(cfg.ckpt_dir, 'dino_vit', f'0{cfg.subject}_all_r_all_0')
-    ckpt_path = os.path.join(ckpt_path, sorted(list(os.listdir(ckpt_path)))[-1])
-
-    model = EncoderModule.load_from_checkpoint(ckpt_path, strict=False).to(cfg.device).eval()
+    model_left = EncoderModule.load_from_checkpoint(ckpt_path_left, strict=False).to(cfg.device).eval()
+    model_right = EncoderModule.load_from_checkpoint(ckpt_path_right, strict=False).to(cfg.device).eval()
 
     subj_folder = os.path.join(cfg.output_dir, f'{cfg.subject}_{cfg.roi}_{cfg.hemisphere}')
     subfolders = sorted([f for f in os.listdir(subj_folder) if os.path.isdir(os.path.join(subj_folder, f))])
 
     for subfolder in tqdm(subfolders, total=len(subfolders)):
 
-        f1 = os.path.join(subj_folder, subfolder, 'dino_vit_preds.npy')
-
-        if os.path.exists(f1):
-            continue
-
         img_list = np.array([f for f in os.listdir(os.path.join(subj_folder, subfolder)) if f.endswith('.png')])
         img_list_order = np.argsort([int(f.replace('.png', '')) for f in img_list])
         img_list = [os.path.join(subj_folder, subfolder, f) for f in img_list[img_list_order]]
 
-        preds = []
+        preds_left = []
+        preds_right = []
         for img in tqdm(img_list):
             img = Image.open(img).convert("RGB")
             img = transform_dino(img).to(cfg.device)
-            pred = model(img.unsqueeze(0)).squeeze(0).detach().cpu().numpy()
-            preds.append(pred)
+            pred_l = model_left(img.unsqueeze(0)).squeeze(0).detach().cpu().numpy()
+            pred_r = model_right(img.unsqueeze(0)).squeeze(0).detach().cpu().numpy()
+            preds_left.append(pred_l)
+            preds_right.append(pred_r)
 
-        preds = np.stack(preds, axis=0).astype(np.float32)
-        preds = preds[:,roi_indices].mean(-1)
-        np.save(f1, preds)
+        preds_left = np.stack(preds_left, axis=0).astype(np.float32)
+        preds_right = np.stack(preds_right, axis=0).astype(np.float32)
+
+        # left rois
+        for roi in ['OFA', 'FFA-1', 'EBA', 'OPA', 'PPA', 'RSC', 'OWFA', 'VWFA-1', 'VWFA-2']:
+            roi_names, roi_classes = parse_rois([roi])
+            roi_indices = get_roi_indices(os.path.join('/gpfs/work5/0/gusr53691/data/NSD/', f"subj{cfg.subject:02d}"), roi_names, roi_classes, 'left')
+            preds = preds_left[:,roi_indices].mean(-1)
+            f = os.path.join(subj_folder, subfolder, f'dino_vit_preds_{roi}_left.npy')
+            np.save(f, preds)
+
+        # right rois
+        for roi in ['OFA', 'FFA-1', 'FFA-2', 'EBA', 'FBA-2', 'OPA', 'PPA', 'RSC', 'OWFA', 'VWFA-1']:
+            roi_names, roi_classes = parse_rois([roi])
+            roi_indices = get_roi_indices(os.path.join('/gpfs/work5/0/gusr53691/data/NSD/', f"subj{cfg.subject:02d}"), roi_names, roi_classes, 'right')
+            preds = preds_right[:,roi_indices].mean(-1)
+            f = os.path.join(subj_folder, subfolder, f'dino_vit_preds_{roi}_right.npy')
+            np.save(f, preds)
+
+
+    print('##############################')
+    print('### Finished ####')
+    print('##############################')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
